8038bbbfdc8c341d9673e80d2a4a9aa825760b60
Jay <dimaondjaylin@gmail.com>
2023-11-02 23:29:08 -0400

    addition of gura figure on table

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
@@ -7,7 +6,7 @@ const Hero = () => {
   return (
     <section className={"relative w-full h-screen mx-auto"}>
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className={"flex flex-col justify-center items-center mt-5"}>
           <div className={"w-5 h-5 rounded-full bg-[#915eff]"}></div>
@@ -18,13 +17,32 @@ const Hero = () => {
             Hi, I'm
             <span className={"text-[#915eff]"}> Jay</span>
           </h1>
-          <p className={`${styles.heroSubText} text-white-100`}>
-            I'm a high school student <br className={"sm:block hidden"} /> who
-            loves to code and design.
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a high school student <br className="sm:block hidden" />
+            who loves to code and design.
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div
+        className={
+          "absolute xs:bottom-1 bottom-32 w-full flex justify-center items-center"
+        }
+      >
+        <a href="#about">
+          <div
+            className={
+              "w-[35px] h-[65px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2"
+            }
+          >
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, loop: Infinity, repeatType: "loop" }}
+              className={"w-3 h-3 rounded-full bg-secondary mb-1"}
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,29 +1,36 @@
-import React, { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
-
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
+      <hemisphereLight intensity={1.75} groundColor={"black"} />
       <pointLight intensity={1} />
+      <spotLight position={[-20, 50, 10]} castShadow />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -3, -1.5] : [0, -3.75, -1.5]}
+        rotation={[0, -0.2, 0]}
+      />
+    </mesh>
+  );
+};
+
+const Gura = ({ isMobile }) => {
+  const object = useGLTF("./gura/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={3} groundColor={"black"} />
+      <primitive
+        object={object.scene}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [-1, -2.85, 2] : [-1.5, -3.5, 4]}
+        rotation={[0, 1.7, 0]}
       />
     </mesh>
   );
@@ -33,31 +40,20 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
+    const mediaQuery = window.matchMedia("(max-width: 600px)");
     setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
-
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
-
   return (
     <Canvas
       frameloop="demand"
       shadows
-      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
@@ -68,6 +64,7 @@ const ComputersCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Computers isMobile={isMobile} />
+        <Gura isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
