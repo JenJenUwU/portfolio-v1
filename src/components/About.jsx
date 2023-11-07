1e78199427f6010a367fe304663190d090886728
Jay <jenjentw358@gmail.com>
2023-11-06 20:22:39 -0500

    adding imports for about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+//import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>About me</p>
+      </motion.div>
+    </>
+  );
+};
 
-export default About
+export default About;
