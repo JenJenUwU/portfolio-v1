e3a33c8c1571e846849647ebc1c1cb101e22cdfd
Jay <jenjentw358@gmail.com>
2023-12-17 23:06:06 -0500

    modified scrolling button position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       <ComputersCanvas />
       <div
         className={
-          "absolute xs:bottom-1 bottom-32 w-full flex justify-center items-center"
+          "absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center"
         }
       >
         <a href="#about">
