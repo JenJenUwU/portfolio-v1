75c5a9ade87a288204d4e441a85b445a2d3a80f5
JenJenUwU <jenjentw358@gmail.com>
2023-12-19 09:26:25 -0500

    updaed island component name and gura's position

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,6 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import IslandCanvas from "./Computers";
+import IslandCanvas from "./Island.jsx";
 import StarsCanvas from "./Stars";
 
-export { EarthCanvas, BallCanvas, IslandCanvas, StarsCanvas };
+export {EarthCanvas, BallCanvas, IslandCanvas, StarsCanvas};
